4d35c980912dc3dca74705a6bbcecc505aba40fa
arm <andy.mcrae21@gmail.com>
2021-05-01 13:24:52 -0500

    stuff is not working

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,44 +1,96 @@
 console.log('logic.js loaded');
 
 // all earthquakes over the last 7-days
-url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
-function drawMap(earthQuakes) {
+// var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     maxZoom: 18,
+//     zoomOffset: -1,
+//     id: "mapbox/light-v10",
+//     accessToken: API_KEY
+// });
+
+// var myMap = L.map("mapid", {
+//     center: [37.09, -95.71],
+//     zoom: 5
+//     // layers: [lightmap] //, earthQuakeLayer]
+// });
+
+// lightmap.addTo(myMap);
+
+
+
+
+
+function drawMap(earthQuakeData) {
     console.log('drawMap');
 
-    var myMap = L.map("mapid", {
-        center: [
-          37.09, -95.71
-        ],
-        zoom: 5,
-        layers: [earthQuakes]
-      });
-
-}
-
-function drawQuakes(earthQuakeData) {
-    console.log('drawQuakes');
 
 
-
-    function onEachFeature(feature, layer) {
+    var earthQuake = function(feature) {
         L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-            stroke: false,
-            fillOpacity: 0.75,
-            fillColor: d3.interpolateRdYlGn(90/feature.geometry.coordinates[2]),
+            fillColor: 'red',
             color: 'black',
-            radius: feature.properties.mag
+            radius: 5000 * feature.properties.mag
         });
-    }
+    } 
 
-    var earthQuakes = L.geoJSON(earthQuakeData, {
-        onEachFeature: onEachFeature
+    // pulled this from inclass example
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/light-v10",
+        accessToken: API_KEY
     });
 
-    drawMap(earthQuakes);
+    var myMap = L.map("mapid", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [lightmap, earthQuakeLayer]
+    });
+
+    lightmap.addTo(myMap);
+
+    L.geoJSON(earthQuake).addTo(myMap);
+
+    var overlayMaps = {
+        Earthquakes: earthQuakeLayer
+      };
+
+      L.control.layers(null, overlayMaps, {
+        collapsed: false
+      }).addTo(myMap);
+
 }
 
-d3.json(url).then(function(data) {
-    drawQuakes(data.features);
-    console.log(data.features)
+// function drawQuakes(earthQuakeData) {
+//     console.log('drawQuakes');
+
+
+//     function drawCircles()
+
+
+//     // function onEachFeature(feature,layer) {
+//     //     L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+//     //         fillColor: 'red',
+//     //         color: 'black',
+//     //         radius: 5000 * feature.properties.mag
+//     //     });
+
+//     // }
+
+//     var earthQuakes = L.geoJSON(earthQuakeData, {
+//         // onEachFeature: onEachFeature
+//     });
+
+//     drawMap(earthQuakes);
+// }
+
+d3.json(url, function(data) {
+    drawMap(data.features);
+    console.log(data.features);
 });
