cb9bfa06a59c041b52e70931a7be012871310bd3
arm <andy.mcrae21@gmail.com>
2021-05-02 16:08:17 -0500

    got legend working right

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -15,44 +15,57 @@ function drawMap(earthQuakes) {
         id: "mapbox/light-v10",
         accessToken: API_KEY
     });
-
+    
+    // making my map
     var myMap = L.map("mapid", {
-        center: [37.09, -95.71],
-        zoom: 4,
+        center: [40.7608, -111.8910],
+        // center: [37.09, -95.71],
+        zoom: 5,
         layers: [lightmap]
     });
-    console.log(typeof(earthQuakes));
+    // console.log(typeof(earthQuakes));
 
-
-    var limits = ['- 10-10', '10-30', '30-50', '50-70', '70-90', '90+'];
+    // making arrays for putting stuff into my legend
+    var limits = ['-10:10', '10:30', '30:50', '50:70', '70:90', '90+'];
     var legendColors = ['#a3f600','#dcf400', '#f7db11', '#fdb72a', '#fca35d', '#ff5f65']
 
+    // making my legend 
     var legend = L.control({position: 'bottomright'});
     legend.onAdd = function() {
         var div = L.DomUtil.create('div', 'info legend');
         var labels = [];
 
-        var legendInfo = "<div class=\"labels\"></div>";
+        var legendInfo = "<ul class=\"labels\">" ;
 
-        div.innerHTML = legendInfo;
+        var legendInfoEnd = "</ul>";
 
         for (i=0; i < limits.length; i++) {
+
+            legendInfo = legendInfo + "<li class=\"label\">" + limits[i] + "</li>"
+
+            // legendInfo = legendInfo + limits[i]
+
             labels.push(
-                "<li style=\"background-color: " + legendColors[i] + "\"></li>" + "<div class=\labels\">" + limits[i] + "</div>"
-                
+                "<li style=\"background-color: " + legendColors[i] + "\"></li>" //+ "<div class=\labels\">" + limits[i] + "</div>"
             );
+
         }
+
+        legendInfo = legendInfo + legendInfoEnd;
+
+        div.innerHTML = legendInfo;
 
         div.innerHTML += "<ul>" + labels.join("") + "</ul>";
         return div;
     };
 
-
+    // adding layers to the map
     lightmap.addTo(myMap);
     earthQuakes.addTo(myMap);
     legend.addTo(myMap);
 
 
+    // adding a control
     var overlayMaps = {
         Earthquakes: earthQuakes
       };
@@ -69,9 +82,12 @@ function drawQuakes(earthQuakeData) {
 
     var colors = ['#a3f600','#dcf400', '#f7db11', '#fdb72a', '#fca35d', '#ff5f65']
 
+    // =================================================
     // TA Chris gave me a nudge onto the right track 
     // for this part and i used microsoft paint to pull
     // hexadecimal colors from image given to us
+
+    // a function that assigns a color to popColor based on depth
     function filling(depth) {
         var popColor = 'black';
         switch (true) {
@@ -97,39 +113,27 @@ function drawQuakes(earthQuakeData) {
 
         return popColor;
     }
+    // ====================================
 
-    var legend = L.control({position: 'bottomright'});
-    legend.onAdd = function() {
-        var div = L.DomUtil.create('div', 'info legend');
-        var labels = [];
-
-        for (i=0; i < limits.length; i++) {
-            labels = labels.push(
-                "<li style=\"background-color: " + legendColors[i] + "\"></li>"
-            );
-        }
-
-        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-        return div;
-    };
-
+    // function for popups on features
     function onEachFeature(feature, layer) {
         layer.bindPopup('<h3>Magnitude: ' + feature.properties.mag + 
             '</h3><h3>Depth: ' + feature.geometry.coordinates[2] +'</h3><hr><p>' + feature.properties.place + '<p>');
     }
 
+    // function for circle styles
     function styleCircles(feature) {
         return {
             fillOpacity: 1,
             stroke: 1,
             fillColor: filling(feature.geometry.coordinates[2]),
             color: 'grey',
-            radius: 3.5 * feature.properties.mag
+            radius: 5 * feature.properties.mag
         }
     }
 
 
-
+    // using geoJSON to add create layers of circles and popups
     // https://leafletjs.com/examples/geojson/
     var earthQuakes = L.geoJSON(earthQuakeData, {
         pointToLayer: function (feature, latlng) {
@@ -137,10 +141,11 @@ function drawQuakes(earthQuakeData) {
         },
         onEachFeature: onEachFeature
     });
-    console.log(typeof(earthQuakes));
+    // console.log(typeof(earthQuakes));
     drawMap(earthQuakes);
 }
 
+// runs functions with json from url
 d3.json(url, function(data) {
     drawQuakes(data.features);
     console.log(data.features);
